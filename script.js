3a7837539b339d1ace01f95a3074c4b25981a230
tommasotacconi <tommaso.tacconi@gmail.com>
2025-12-02 05:14:02 +0100

    feat: Create base skeleton with html and then js, usescss for 3D effects and colors' animations

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log('js ok');
+
+const body = document.body;
+// Prepare cell for mapping mouse position
+const bodyInHTML = body.innerHTML;
+body.innerHTML = '';
+
+for (let i = 0; i < 250; i++) {
+    const cellEl = '<div class="cell"></div>';
+    
+    if (i < 249) body.innerHTML += cellEl;
+    else body.innerHTML += cellEl + bodyInHTML;
+}
+
+const greetsSec = document.getElementById('greetings');
+const greetsSecInHTML = greetsSec.innerHTML;
+let agePart = 0;
+let textPart = agePart;
+const getHappyBdayClassEl = (text, addedClasses) => `<div class="happy-birthday ${addedClasses}">${text}</div>`;
+let happyBdayClassEl = getHappyBdayClassEl(textPart, 'box-18');
+
+for (let i = 1; i <= 18; i++) {
+    setTimeout(() => {
+        agePart++;
+        textPart = 'Emma!!!';
+        happyBdayClassEl = getHappyBdayClassEl(textPart);
+        greetsSec.innerHTML = greetsSecInHTML + Array(i).fill(happyBdayClassEl).join("") + getHappyBdayClassEl(agePart, 'box-18');
+    }, 10000 * i ** (1 / 4));
+    console.log(400 * i ** (1 / 2));
+}
+greetsSec.innerHTML += happyBdayClassEl; 
