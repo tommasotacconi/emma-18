fe0aeb6fd83739e34853e279989015b06b7b6d9f
tommasotacconi <tommaso.tacconi@gmail.com>
2025-12-04 03:52:26 +0100

    feat: Add more fluid animation, add transitions between background's gradients, add pseudo-element border and remove main background to enter in final state

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-console.log('js ok');
-
 const body = document.body;
 // Prepare cell for mapping mouse position
 const bodyInHTML = body.innerHTML;
+const compDocumentSt = document.documentElement;
 body.innerHTML = '';
 
 for (let i = 0; i < 250; i++) {
@@ -16,19 +15,40 @@ const greetsSec = document.getElementById('greetings');
 const greetsSecInHTML = greetsSec.innerHTML;
 let agePart = 0;
 let textPart = agePart;
-const getHappyBdayClassEl = (text, addedClasses) => `<div class="happy-birthday ${addedClasses}">${text}</div>`;
-let happyBdayClassEl = getHappyBdayClassEl(textPart, 'box-18');
+const getHappyBdayClassEl = (text, addedClasses = '') => {
+    const divEl = document.createElement('div');
+    divEl.classList = 'happy-birthday ' + addedClasses;
+    divEl.innerText = text
+    
+    return divEl;
+};
 
 for (let i = 1; i <= 18; i++) {
     setTimeout(() => {
-        agePart++;
         textPart = 'Emma!!!';
         happyBdayClassEl = getHappyBdayClassEl(textPart);
-        greetsSec.innerHTML = greetsSecInHTML + Array(i).fill(happyBdayClassEl).join("") + getHappyBdayClassEl(agePart, 'box-18');
+        lastChildInGreets = greetsSec.lastChild;
+        lastChildInGreets.innerText = ++agePart;
+        greetsSec.insertBefore(happyBdayClassEl, lastChildInGreets);
+        if (i === 18) {
+            const { style: lastChildSt, classList } = lastChildInGreets;
+            let animationTime = 0.4;
+            compDocumentSt.style.setProperty('--animation-time', animationTime + 's');
+            setTimeout(() => {
+                classList.add('no-gradient-border');
+                setTimeout(() => {
+                    classList.add('onbefore-gradient-border')
+                    animationTime = 15;
+                    compDocumentSt.style.setProperty('--animation-time', animationTime + 's');
+                }, 4000);
+                // lastChildSt.animationDuration = '30s';
+            }, 8000);
+        } 
+            
     }, 10000 * i ** (1 / 4));
-    console.log(400 * i ** (1 / 2));
 }
-greetsSec.innerHTML += happyBdayClassEl; 
+let happyBdayClassEl = getHappyBdayClassEl(textPart, 'box-18');
+greetsSec.innerHTML += happyBdayClassEl.outerHTML; 
 
 
 // Copyright update
