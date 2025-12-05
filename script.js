2699e3081f7ca6c6723500269d953451ab380cc2
tommasotacconi <tommaso.tacconi@gmail.com>
2025-12-05 18:13:56 +0100

    refactor, style: Remove hardcoded base text for greetings, use span as greetings words wrapper; use function for greets animation; change age box's class, update css; organize code style

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 /* Retrieve DOM elementS */
 const body = document.body;
 const bodyInHTML = body.innerHTML;
-const documentEl = document.documentElement;
 
 /* Prepare cells for mapping mouse position */
 body.innerHTML = '';
@@ -16,50 +15,60 @@ for (let i = 0; i < 250; i++) {
 /* Add age in front of '.greetings' text and a series of birthday person's names
 as long as the age. This is sequential, hence create steps where you increment
 age count and add a name  */  
-const greetsSec = document.getElementById('greetings');
-const greetsSecInHTML = greetsSec.innerHTML;
+const greets = document.getElementById('greetings');
 let agePart = 0;
 let textPart = agePart;
 const getHappyBdayClassEl = (text, addedClasses = '') => {
-    const divEl = document.createElement('div');
-    divEl.classList = 'happy-birthday ' + addedClasses;
-    divEl.innerText = text
+    const spanEl = document.createElement('span');
+    spanEl.classList = 'happy-birthday ' + addedClasses;
+    spanEl.innerText = text
     
-    return divEl;
+    return spanEl;
 };
 
-// Insert age count
-let happyBdayClassEl = getHappyBdayClassEl(textPart, 'box-18');
-greetsSec.innerHTML += happyBdayClassEl.outerHTML;
-
-for (let i = 1; i <= 18; i++) {
-    setTimeout(() => {
-        // Insert name and update age
-        textPart = 'Emma!!!';
-        happyBdayClassEl = getHappyBdayClassEl(textPart);
-        lastChildInGreets = greetsSec.lastChild;
-        lastChildInGreets.innerText = ++agePart;
-        greetsSec.insertBefore(happyBdayClassEl, lastChildInGreets);
-        // Start a faster animation for age box background animation
-        // and after completion remove background and show pseudo-element
-        // for lighter and slower animation (border only animation) 
-        if (i === 18) {
-            const { classList } = lastChildInGreets;
-            let animationTime = 0.4;
-            documentEl.style.setProperty('--animation-time', animationTime + 's');
-            setTimeout(() => {
-                classList.add('no-gradient-border');
+function makeGreets(toPerson, hadAge, greetings) {
+    // Insert greetings
+    greetings.split(" ").forEach(greetsWord => {
+        const el = getHappyBdayClassEl(greetsWord);
+        greets.innerHTML += el.outerHTML;
+    })
+    // Insert age count
+    let happyBdayClassEl = getHappyBdayClassEl(textPart, 'had-age-box');
+    greets.innerHTML += happyBdayClassEl.outerHTML;
+    
+    for (let i = 1; i <= hadAge; i++) {
+        setTimeout(() => {
+            // Insert name and update age
+            textPart = toPerson + '!!!';
+            happyBdayClassEl = getHappyBdayClassEl(textPart);
+            lastChildInGreets = greets.lastChild;
+            lastChildInGreets.innerText = ++agePart;
+            greets.insertBefore(happyBdayClassEl, lastChildInGreets);
+            // Start a faster animation for age box background animation
+            // and after completion remove background and show pseudo-element
+            // for lighter and slower animation (border only animation)
+            if (i === hadAge) {
+                const { classList } = lastChildInGreets;
+                let animationTime = 0.4;
+                document.documentElement.style.setProperty('--animation-time', animationTime + 's');
                 setTimeout(() => {
-                    classList.add('onbefore-gradient-border')
-                    animationTime = 15;
-                    documentEl.style.setProperty('--animation-time', animationTime + 's');
-                }, 4000);
-            }, 8000);
-        } 
-    }, 10000 * i ** (1 / 4));
+                    classList.add('no-gradient-border');
+                    setTimeout(() => {
+                        classList.add('onbefore-gradient-border')
+                        animationTime = 15;
+                        document.documentElement.style.setProperty('--animation-time', animationTime + 's');
+                    }, 4000);
+                }, 8000);
+            }
+        }, 10000 * i ** (1 / 4));
+    }
+
+    greets.style.display = 'block';
 }
 
 // Copyright date update
 const currentYear = new Date().getFullYear();
 const crYear = document.getElementsByClassName('cr-year')[0];
 crYear.innerText = currentYear;
+
+makeGreets('Emma', 18, 'Tanti auguri di buon compleanno');
